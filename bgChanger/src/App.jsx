36d73e99d43c3c9c54e4dfe65dbe34b7bd2f7752
SadN00dle <sadnoodle11@gmail.com>
2025-06-03 20:23:48 +0545

    finish background changer

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,13 +1,33 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [bgColor, setBgColor] = useState('white')
-  
+  let [bgColor, setBgColor] = useState('white')
+
   return (
     <>  
-      <button className='bg-black text-white p-2 rounded-md m-2' onClick={() => setBgColor('black')}>Black</button>
-      <br />
-      <button className='bg-white text-black p-2 rounded-md m-2' onClick={() => setBgColor('white')}>White</button>
+      <div className='w-full h-screen duration-200 flex flex-wrap justify-center items-end'   
+      style={{backgroundColor: bgColor}}>
+        <button className='bg-black text-white p-2 rounded-md m-2 mb-5'
+        onClick={() => setBgColor('black')}>Black</button>
+
+        <br />
+
+        <button className='bg-white text-black p-2 rounded-md m-2 mb-5'
+         onClick={() => setBgColor('white')}>White</button>
+        
+        <br />
+
+        <button className='bg-blue-200 text-black p-2 rounded-md m-2 mb-5'
+         onClick={() => setBgColor('oklch(88.2% 0.059 254.128)')}>Ice Blue</button>
+        
+        <br />
+
+        <button className='bg-yellow-400 text-violet-700 p-2 rounded-md m-2 mb-5'
+         onClick={() => setBgColor('oklch(85.2% 0.199 91.936)')}>Golden</button>
+         
+      </div>
+     
     </>
   )
 }
