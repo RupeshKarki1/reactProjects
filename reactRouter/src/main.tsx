fbb6cd27163240310d7b88c24b947451dd43fb2d
SadN00dle <sadnoodle11@gmail.com>
2025-06-06 19:57:18 +0545

    highlight on click and about page set

--- reactRouter/src/main.tsx
+++ reactRouter/src/main.tsx
@@ -1,10 +1,50 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router'
+import { RouterProvider } from 'react-router-dom'
+
+import Home from './components/Home/Home'
+import Layout from './Layout'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />
+//       },
+//       {
+//         path: 'about',
+//         element: <About />  
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />
+//       }
+
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<Layout />}>
+      <Route path='/' element = {<Home />} />
+      <Route path='/about' element = {<About />} />
+      <Route path='/contact' element = {<Contact />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
