0d668ec0db3f3459e240685cb79740640296371d
SadN00dle <sadnoodle11@gmail.com>
2025-06-07 11:55:02 +0545

    make with jsx instead of tsx

---
+++ contextAPI/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Login from './components/Login'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Login />
+    </UserContextProvider>
+  )
+}
+
+export default App
