17ac83a96de5802209654c7d9dbb9a10907d65a1
SadN00dle <sadnoodle11@gmail.com>
2025-06-09 12:54:28 +0545

    made reducer and todos jsx

--- reduxTKtodo/src/App.jsx
+++ reduxTKtodo/src/App.jsx
@@ -1,12 +1,10 @@
+import Todos from "./components/Todos"
 
 function App() {
   return (
-   <>
-   <div className="h-screen w-full bg-amber-200">
-    <h1 className='text-center text-4xl'>test</h1>
-   </div>
-    
-  </>
+    <>
+      <Todos />
+    </>
   )
 }
 
